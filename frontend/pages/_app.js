aeadc3ea724523ee125712adcbfd426933d4155f
Matej Gerek <matej.gerek@student.tuke.sk>
2022-11-05 04:11:18 +0100

    Add mqtt

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -6,11 +6,31 @@ const roboto = Roboto({
     weight: '400'
 })
 
+import mqtt from 'mqtt'
+import { useEffect, useState } from 'react'
+
 function MyApp({ Component, pageProps }) {
+    const [cardId, setCardId] = useState()
+
+    useEffect(() => {
+        const client = mqtt.connect('tcp://broker.hivemq.com:8000/mqtt', {})
+
+        client.on('connect', function () {
+            client.subscribe('hackathon/kajmmi/rfid/pay')
+        })
+
+        client.on('message', function (topic, message) {
+            // message is Buffer
+            setCardId(message.toString())
+            console.log(message.toString())
+            //   client.end();
+        })
+    }, [])
+
     return (
         <ChakraProvider>
             <main className={roboto.className}>
-                <Component {...pageProps} />
+                {cardId ? <Component {...pageProps} /> : 'no card id'}
             </main>
         </ChakraProvider>
     )
